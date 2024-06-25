1875691a9665068b553efd31ebfbf412b15d0e67
IFHAL FAIZI <ifhal.535230196@stu.untar.ac.id>
2024-06-26 00:28:58 +0700

    IFHAL- membuat layo template Blog menggunakan bootstrap bawaan dari google

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,22 @@
 <?php
-
-use App\Http\Controllers\HomeController;
-use Illuminate\Support\Facades\{Route, Auth};
-use App\Http\Controllers\LandingPageController;
 use App\Http\Controllers\Back\ArticleController;
 use App\Http\Controllers\Back\DashboardController;
 use App\Http\Controllers\Back\CategoryController; // Add this line
 use App\Http\Controllers\Back\UserController;
 
-Auth::routes();
+use App\Http\Controllers\Front\HomeController;
+use Illuminate\Support\Facades\{Route, Auth};
+use App\Http\Controllers\LandingPageController;
 
-Route::get('/dashboard', [HomeController::class, 'index'])->name('home');
 
-Route::get('/', [LandingPageController::class, 'index']);
+
+// Route::get('/dashboard', [HomeController::class, 'index'])->name('home');
+
+// Route::get('/', [LandingPageController::class, 'index']);
+
+Route::get('/', [HomeController::class, 'index']);
+
+
 
 Route::middleware('auth')->group(function() {
     Route::get('/dashboard',[DashboardController::class, 'index']);
@@ -29,9 +33,9 @@ Route::middleware('auth')->group(function() {
 
     Route::group(['prefix' => 'laravel-filemanager'], function () {
     \UniSharp\LaravelFilemanager\Lfm::routes();
+    });
 });
-
-});
-
 
 Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
